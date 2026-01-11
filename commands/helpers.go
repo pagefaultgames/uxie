e50a468ac449a353cf4c90bd3754ca11ee671e52
Bertie690 <taylormw163@gmail.com>
2026-01-11 00:33:36 -0500

    remove unused func

--- commands/helpers.go
+++ commands/helpers.go
@@ -24,24 +24,3 @@ func getTextInputValue(
 	}
 	return ""
 }
-
-// getStringSelectValue is a helper function to extract a string select modal's value from within a label.
-// It returns the chosen option, or an empty string if absent.
-func getStringSelectValue(
-	itx *tempest.ModalInteraction,
-	customId string,
-) (contents []string) {
-	for _, comp := range itx.Data.Components {
-		label, ok := comp.(tempest.LabelComponent)
-		if !ok {
-			continue
-		}
-		// NB: Golang type assertions produce zero values if type assertion fails
-		c, found := label.Component.(tempest.StringSelectComponent)
-		if !found || c.CustomID != customId {
-			continue
-		}
-		return c.Values
-	}
-	return nil
-}
