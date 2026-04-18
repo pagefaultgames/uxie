cc6b0d55e2ac896380fd934cc3d8041a9b79f929
Bertie690 <taylormw163@gmail.com>
2026-04-18 17:02:58 -0400

    feat: Make the repo REUSE compliant

--- db/doc.go
+++ db/doc.go
@@ -1,3 +1,8 @@
+// SPDX-FileCopyrightText: 2026 Pagefault Games
+// SPDX-FileContributor: Bertie690
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 // Package db provides functionality related to the bot's help topic database.
 // It contains the database with help messages as well as a simple API to manipulate it.
 package db
