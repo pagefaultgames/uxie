37c074b0347f73ca4534fbdf74d4a3a804a2e42c
Bertie690 <taylormw163@gmail.com>
2026-05-07 18:29:42 -0400

    refactor: remove prepared statements

--- db/store.go
+++ db/store.go
@@ -22,36 +22,8 @@ type Store struct {
 	// The underlying database connection.
 	db *sql.DB
 
-	// A map containing prepared statements for common queries.
-	statements map[statementName]*sql.Stmt
-
 	// TODO: Pass in top level context from main function (for cancellation on shutdown)
 }
-
-// statementName is a string enum used for identifying prepared MariaDB statements.
-type statementName string
-
-// NB: These statements always return `name` due to case folding potentially making "FOO" match "foo"
-
-const (
-	// Get a single help topic.
-	//  `SELECT id, name, text, updated_at FROM topics WHERE name = ?`
-	getHelpTopic statementName = "getHelpTopic"
-	// Get all help topics.
-	//  `SELECT id, name, text, updated_at FROM topics`
-	getAllTopics statementName = "getAllTopics"
-	// Add a new help topic to the database, using the current time as a timestamp.
-	//  `INSERT INTO topics (name, text) VALUES (?, ?)`
-	addHelpTopic statementName = "addHelpTopic"
-	// Modify an existing help topic in the database, using the provided timestamp to verify correctness.
-	//  `UPDATE topics
-	//    SET text = ?, updated_at = CURRENT_TIMESTAMP(6)
-	//    WHERE name = ? AND updated_at = ?`
-	updateHelpTopic statementName = "updateHelpTopic"
-	// Delete a help topic.
-	//  `DELETE FROM topics WHERE name = ? RETURNING id, name, text, updated_at`
-	deleteTopic statementName = "deleteTopic"
-)
 
 // A topicId is a reference to a [HelpTopic]'s rowid, used for clarity of intent.
 type topicId = int64
@@ -100,72 +72,15 @@ func (s *Store) init(ctx context.Context) error {
 		return fmt.Errorf("failed to run database migrations: %w", err)
 	}
 
-	return s.prepareStatements(ctx)
-}
-
-// prepareStatements prepares commonly used SQL statements.
-func (s *Store) prepareStatements(ctx context.Context) (err error) {
-	s.statements = make(map[statementName]*sql.Stmt, 3)
-	s.statements[getHelpTopic], err = s.db.PrepareContext(ctx, `
-		SELECT id, name, text, updated_at FROM topics WHERE name = ?
-	`)
-	if err != nil {
-		return fmt.Errorf("failed to prepare getHelpTopic statement: %w", err)
-	}
-
-	s.statements[addHelpTopic], err = s.db.PrepareContext(ctx, `
-		INSERT INTO topics (name, text) VALUES (?, ?)
-	`)
-	if err != nil {
-		return fmt.Errorf("failed to prepare addHelpTopic statement: %w", err)
-	}
-
-	s.statements[updateHelpTopic], err = s.db.PrepareContext(ctx, `
-		  UPDATE topics
-		  SET text = ?, updated_at = CURRENT_TIMESTAMP(6)
-		  WHERE name = ? AND updated_at = ?
-	`)
-	if err != nil {
-		return fmt.Errorf("failed to prepare updateHelpTopic statement: %w", err)
-	}
-
-	s.statements[getAllTopics], err = s.db.PrepareContext(ctx, `
-		SELECT id, name, text, updated_at FROM topics
-	`)
-	if err != nil {
-		return fmt.Errorf("failed to prepare getAllTopics statement: %w", err)
-	}
-
-	s.statements[deleteTopic], err = s.db.PrepareContext(ctx, `
-		DELETE FROM topics WHERE name = ? RETURNING id, name, text, updated_at
-	`)
-	if err != nil {
-		return fmt.Errorf("failed to prepare deleteTopic statement: %w", err)
-	}
 	return nil
 }
 
-func (s *Store) close() (err error) {
-	for name, stmt := range s.statements {
-		if stmtCloseErr := stmt.Close(); err != nil {
-			utils.ErrorAttrs(
-				"Failed to close prepared statement",
-				slog.String("name", string(name)),
-				slog.Any("error", stmtCloseErr),
-			)
-			err = errors.Join(
-				err,
-				fmt.Errorf("failed to close prepared statement %q: %w", name, stmtCloseErr),
-			)
-		}
+func (s *Store) close() error {
+	if err := s.db.Close(); err != nil {
+		utils.ErrorAttrs("failed to close database", slog.Any("error", err))
+		return fmt.Errorf("failed to close database: %w", err)
 	}
-
-	if closeErr := s.db.Close(); closeErr != nil {
-		utils.ErrorAttrs("failed to close database", slog.Any("error", closeErr))
-		err = errors.Join(err, fmt.Errorf("failed to close database: %w", closeErr))
-	}
-
-	return err
+	return nil
 }
 
 func (s *Store) getHelpTopic(name string) (topic HelpTopic, err error) {
@@ -174,7 +89,9 @@ func (s *Store) getHelpTopic(name string) (topic HelpTopic, err error) {
 		text      string
 		updatedAt time.Time
 	)
-	err = s.statements[getHelpTopic].QueryRow(name).Scan(&id, &name, &text, &updatedAt)
+	err = s.db.QueryRow(`
+		SELECT id, name, text, updated_at FROM topics WHERE name = ?
+	`, name).Scan(&id, &name, &text, &updatedAt)
 	if err != nil {
 		return HelpTopic{}, fmt.Errorf("failed to get help topic %q: %w", name, err)
 	}
@@ -187,7 +104,9 @@ func (s *Store) getHelpTopic(name string) (topic HelpTopic, err error) {
 }
 
 func (s *Store) getAllTopics() (topics []HelpTopic, err error) {
-	rows, err := s.statements[getAllTopics].Query()
+	rows, err := s.db.Query(`
+		SELECT id, name, text, updated_at FROM topics
+	`)
 	if err != nil {
 		return nil, fmt.Errorf("failed to query help topics: %w", err)
 	}
@@ -222,7 +141,9 @@ func (s *Store) getAllTopics() (topics []HelpTopic, err error) {
 // addHelpTopic adds a new help topic with the given name and text to the database.
 // If a topic with the same name already exists, it will remain unchanged and the underlying [mysql.MySQLError] will be returned.
 func (s *Store) addHelpTopic(name, text string) error {
-	_, err := s.statements[addHelpTopic].Exec(name, text)
+	_, err := s.db.Exec(`
+		INSERT INTO topics (name, text) VALUES (?, ?)
+	`, name, text)
 	if err != nil {
 		return fmt.Errorf("failed to add help topic %q to database: %w", name, err)
 	}
@@ -239,7 +160,11 @@ func (s *Store) updateHelpTopic(
 	name, text string,
 	expectedUpdatedAt time.Time,
 ) error {
-	result, err := s.statements[updateHelpTopic].Exec(text, name, expectedUpdatedAt)
+	result, err := s.db.Exec(`
+		  UPDATE topics
+		  SET text = ?, updated_at = CURRENT_TIMESTAMP(6)
+		  WHERE name = ? AND updated_at = ?
+	`, text, name, expectedUpdatedAt)
 	if err != nil {
 		return fmt.Errorf("failed to update help topic %q in database: %w", name, err)
 	}
@@ -274,7 +199,9 @@ func (s *Store) updateHelpTopic(
 // deleteTopic deletes the help topic with the given name, returning the deleted topic and any error produced.
 // If no topic with the given name exists, an error implementing [sql.ErrNoRows] will be returned.
 func (s *Store) deleteTopic(topicName string) (HelpTopic, error) {
-	row := s.statements[deleteTopic].QueryRow(topicName)
+	row := s.db.QueryRow(`
+		DELETE FROM topics WHERE name = ? RETURNING id, name, text, updated_at
+	`, topicName)
 	var (
 		id        topicId
 		text      string
