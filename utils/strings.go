6e90554d4312fb783b49d76ba31f17d02924d299
Bertie690 <taylormw163@gmail.com>
2026-05-08 16:34:34 -0400

    Add license information

--- utils/strings.go
+++ utils/strings.go
@@ -1,3 +1,8 @@
+// SPDX-FileCopyrightText: 2026 Pagefault Games
+// SPDX-FileContributor: Bertie690
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 package utils
 
 import (
@@ -5,6 +10,7 @@ import (
 	"golang.org/x/text/language"
 )
 
+// TitleCase converts a string to title case.
 func TitleCase(s string) string {
 	caser := cases.Title(language.English)
 	return caser.String(s)
